0e98ef55bc548843e5a2648c5262b8076c34ba79
Slim Amamou <slim.amamou@gmail.com>
2009-07-22 13:18:37 +0100

    Edit source button in the bookmarklet

--- form/index.php
+++ form/index.php
@@ -10,12 +10,12 @@ textarea:focus {
 </head>
 <body>
 <div id="entete"><h1>Make a bookmarklet</h1></div>
-<form action="http://bookmarklet.alixsys.com" method="get">
-  <label>Label<input name="t" type="text" /></label>
+<form action=".." method="get">
+  <label>Label<input name="t" type="text"  value="<?php print stripslashes($_GET['t']) ?>" /></label>
   <label>Code<textarea name="j" rows="24" cols="80">
-document.loadscript("");
+<?php print $_GET['j'] ? stripslashes($_GET['j']) : 'document.loadscript("");'; ?>
   </textarea></label>
-  <label>Description<input name="d" type="text" size="50" value="Drag'n'Drop the bookmarklet on your browser toolbar." /></label>
+  <label>Description<input name="d" type="text" size="50" value="<?php print $_GET['d'] ? stripslashes($_GET['d']) : "Drag'n'Drop the bookmarklet on your browser toolbar." ?>" /></label>
   <button type="submit">Make a bookmarklet</button>
 </form>
 </body> 
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <html>
 <head>
 <title><?php echo $_GET['t'] ?> bookmarklet</title>
-<link type="text/css" rel="stylesheet" media="all" href="arabtechies.css" />
 </head>
 <style>
 body {
 	text-align: center;
+	background-color: #E6E6E6;
+	color:#494949;
+	font-family:Verdana,Arial,Helvetica,sans-serif;
+	font-size:76%;
 }
 .bubble {
 	width: auto;
@@ -35,10 +38,20 @@ h1 {
 	color: inherit;
 	text-decoration: inherit;
 }
+a.editsource {
+	position: fixed;
+	bottom: 3px;
+	right: 3px;
+	text-decoration: none;
+	background-color: green;
+	color: white;
+	padding: 3px 5px;
+}
 </style>
 <body>
 <h1><?php print stripslashes($_GET['t']) ?> bookmarklet</h1>
 <a class="bookmarklet" href="javascript:document.loadscript=function(u){var%20es=['body','frameset','head'];try{var%20s=document.createElement('script');s.setAttribute('src',u);for%20(var%20i=0;i<es.length;i++)%20{var%20e=document.getElementsByTagName(es[i])[0];if(e){e.appendChild(s);break;}}}catch(e){alert('This%20doesn\'t%20work%20here.');}void(0);};<?php print stripslashes($_GET['j']) ?>" /><?php print stripslashes($_GET['t']) ?></a>
 <p class="bubble"><cite><?php print stripslashes($_GET['d']) ?></cite></p>
+<a class="editsource" href="<?php print "form/?". $_SERVER['QUERY_STRING'] ?>">Edit the source code of this bookmarklet</a>
 </body>
 </html>
